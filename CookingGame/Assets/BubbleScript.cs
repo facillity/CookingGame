2426916cb8962220b3b609a927d5c19908ce901b
facillity <ignacia@uci.edu>
2019-02-20 23:48:22 -0800

    Krusty Krew Intro Screen
    Added intro screen, still needs audio!

---
+++ CookingGame/Assets/BubbleScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleScript : MonoBehaviour
+{
+    public float speed = 4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * Time.deltaTime * speed, Space.World);
+        if (transform.position.y > 10){
+            Destroy(gameObject);
+        }
+    }
+}
