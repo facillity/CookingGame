463cce3c78b949fa09273aa6826c4429f18f30fe
facillity <ignacia@uci.edu>
2019-02-24 19:39:45 -0800

    Added Settings, Cookbook, and merging inbetween scenes. Also Fonts

---
+++ CookingGame/Assets/Scripts/IntroLogic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroLogic : MonoBehaviour
+{
+    float timer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        //Debug.Log(timer);
+        if (timer > 7.75f){
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
