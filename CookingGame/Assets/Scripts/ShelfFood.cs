19d6e368b19a2114abf3228e00fc2783d9f778b7
Darrachan Helm <dghelm@uci.edu>
2019-02-26 00:54:47 -0800

    changed shopping list
    added the salmon nigiri ingredients to the shopping list

--- CookingGame/Assets/Scripts/ShelfFood.cs
+++ CookingGame/Assets/Scripts/ShelfFood.cs
@@ -25,15 +25,21 @@ public class ShelfFood : MonoBehaviour
     public GameObject food6;
     public GameObject food7;
     public GameObject food8;
+    public GameObject food9;
+    public GameObject food10;
+    public GameObject food11;
+    public GameObject food12;
 
     int shoppingListLength;
-    int foodListLength = 8;
+    int foodListLength = 12;
     public static bool doneShopping = false;
 
     public Text item1;
     public Text item2;
     public Text item3;
     public Text item4;
+    public Text item5;
+    public Text item6;
 
     // Start is called before the first frame update
     void Start()
@@ -42,9 +48,9 @@ public class ShelfFood : MonoBehaviour
         foodCount = 0;
         //food = new GameObject[25];
         Invoke("StockShelf", 0.4f);
-        foodList = new List<GameObject> { food1, food2, food3, food4, food5, food6, food7, food8 };
-        shoppingList = new string[4]{"pepper(Clone)", "vinegar(Clone)", "salt(Clone)", "pie(Clone)"};
-        quantity = new int[4] { 1, 2 , 3, 2 };
+        foodList = new List<GameObject> { food1, food2, food3, food4, food5, food6, food7, food8, food9, food10, food11, food12 };
+        shoppingList = new string[6]{"soy sauce(Clone)", "vinegar(Clone)", "salt(Clone)", "sushi rice bag(Clone)", "wasabi(Clone)","salmonslab(Clone)"};
+        quantity = new int[6] { 4, 3 , 3, 5, 2, 4 };
 
     }
 
@@ -55,7 +61,7 @@ public class ShelfFood : MonoBehaviour
        if (doneShopping)
         {
             Debug.Log("done");
-            GameObject.Find("Progress").GetComponent<ProgressScript>().stage++;
+          //  GameObject.Find("Progress").GetComponent<ProgressScript>().stage++;
             SceneManager.LoadScene("SalmonNigiriRecipe");
             Destroy(gameObject) ;
         }
@@ -82,12 +88,14 @@ public class ShelfFood : MonoBehaviour
 
     private void UpdateList()
     {
-        item1.text =  quantity[0].ToString() + " bottle of pepper";
+        item1.text =  quantity[0].ToString() + " bottle of soy sauce";
         item2.text = quantity[2].ToString() + " bottles of salt";
 
-        item3.text = quantity[3].ToString() + " pies";
+        item3.text = quantity[3].ToString() + " bags of sushi rice";
 
-        item4.text = quantity[1].ToString() + " bottle of vinegar";
+        item4.text = quantity[5].ToString() + " pieces of salmon";
+        item5.text = quantity[4].ToString() + " bottles of wasabi";
+        item6.text = quantity[1].ToString() + " bottle of vinegar";
 
     }
 
