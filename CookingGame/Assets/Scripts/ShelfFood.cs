d36737bd02b0165f4d6407b7ff8a2ac4002c6527
Darrachan Helm <dghelm@uci.edu>
2019-02-26 11:33:26 -0800

    pics instead of words

--- CookingGame/Assets/Scripts/ShelfFood.cs
+++ CookingGame/Assets/Scripts/ShelfFood.cs
@@ -50,7 +50,7 @@ public class ShelfFood : MonoBehaviour
         Invoke("StockShelf", 0.4f);
         foodList = new List<GameObject> { food1, food2, food3, food4, food5, food6, food7, food8, food9, food10, food11, food12 };
         shoppingList = new string[6]{"soy sauce(Clone)", "vinegar(Clone)", "salt(Clone)", "sushi rice bag(Clone)", "wasabi(Clone)","salmonslab(Clone)"};
-        quantity = new int[6] { 4, 3 , 3, 5, 2, 4 };
+        quantity = new int[6] { 4, 3 , 3, 4, 2, 4 };
 
     }
 
