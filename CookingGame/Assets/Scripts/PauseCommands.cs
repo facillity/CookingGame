02a25660677ceea54c745594a4ad4f5294d6f37e
Kian Thompson <kian.s.thompson@gmail.com>
2019-02-26 17:44:20 -0800

    pause menu for main screen

---
+++ CookingGame/Assets/Scripts/PauseCommands.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseCommands : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Resume()
+    {
+        AudioListener.volume = 1;
+        pauseMenu.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
