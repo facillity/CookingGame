b838fd590e52011984a770e870ac796e4859cf37
Darrachan Helm <dghelm@uci.edu>
2019-03-08 17:01:35 -0800

    Shrimp Tempura Batter Mini Game

--- CookingGame/Assets/Scripts/DrawLine.cs
+++ CookingGame/Assets/Scripts/DrawLine.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class DrawLine : MonoBehaviour
 {
+    public LineRenderer lineRender;
+    public EdgeCollider2D edgeCollider;
+
+    List<Vector2> points;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,23 @@ public class DrawLine : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void DrawLines(Vector2 point)
+    {
+        if (points == null)
+        {
+            points = new List<Vector2>();
+      //      SetPoint();
+
+        }
+    }
+
+    void SetPoint(Vector2 point)
+    {
+        points.Add(point);
+     //   lineRender.numsPositions = points.Count;
+        lineRender.SetPosition(points.Count - 1, point);
+        edgeCollider.points = points.ToArray();
     }
 }
