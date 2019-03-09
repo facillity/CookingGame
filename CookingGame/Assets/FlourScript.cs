a026f9e461f52b6a8c99c7a9656d90a0028a7542
Darrachan Helm <dghelm@uci.edu>
2019-03-09 13:34:32 -0800

    shrimp tempura flour game

---
+++ CookingGame/Assets/FlourScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<ParticleSystem>().emissionRate = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<ParticleSystem>().emissionRate = 10;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            GetComponent<ParticleSystem>().emissionRate = 0;
+        }
+    }
+}
