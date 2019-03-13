f5afb8af18a7bd176afa18a1fc5cc0d990dbb2b2
Kian Thompson <kian.s.thompson@gmail.com>
2019-03-12 23:52:47 -0700

    Breakfast memory game

---
+++ CookingGame/Assets/BreakfastMemoryButtonLogic.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakfastMemoryButtonLogic : MonoBehaviour
+{
+    public void addSausage()
+    {
+        BreakfastMemoryGameLogic.addLeft();
+    }
+
+    public void addEgg()
+    {
+        BreakfastMemoryGameLogic.addRight();
+    }
+}
